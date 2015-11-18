7b479e7bd910100a03fe310baeae07f0ea4f0480
meng <pingf0@gmail.com>
2015-11-18 22:48:32 +0800

    update context logger

--- logging/logger_reload_with_context.py
+++ logging/logger_reload_with_context.py
@@ -6,23 +6,35 @@ yaml_config('yaml.conf')
 
 
 class ContextLogger(object):
-    def __init__(self, name):
+    USELESS_STACK = -3
+
+    def __init__(self, name, project_path=None):
         self.logger = logging.getLogger(name)
+        self.path = project_path
 
-    def _context(self):
+    def _context(self, more=False):
         stack = traceback.extract_stack()
-        (filename, line, procname, text) = stack[-3]
-        print(stack)
+        (filename, line, procname, text) = stack[self.USELESS_STACK]
+        filename = self._filename(filename)
+        steps = [self._filename(filename) + ':' + str(line) + '#' + mod + ':' + ctx
+                 for filename, line, mod, ctx in stack[:self.USELESS_STACK]]
+        stack = '  '.join(steps)
+        context = '  [loc] ' + filename + ':' + procname + ':' + str(line)
+        return context + '  [stk] ' + stack if more else context
 
-        steps = [mod + ':' + str(line) + ':' + ctx for _, line, mod, ctx in stack[:-3]]
-        stack = '>>'.join(steps)
-        return '  [loc] ' + filename + ':' + procname + ':' + str(line) + '  [stk] ' + stack
+    def _filename(self, filename):
+        if self.path is not None and filename.find(self.path) > -1:
+            if self.path == '/':
+                return filename[filename.rfind('/') + 1:]
+            else:
+                filename = filename[len(self.path):]
+                return filename[1:] if filename.find('/') == 0 else filename
 
     def critical(self, msg):
-        self.logger.critical('[msg] ' + str(msg) + self._context())
+        self.logger.critical('[msg] ' + str(msg) + self._context(1))
 
     def error(self, msg):
-        self.logger.error('[msg] ' + str(msg) + self._context())
+        self.logger.error('[msg] ' + str(msg) + self._context(1))
 
     def warning(self, msg):
         self.logger.warning('[msg] ' + str(msg) + self._context())
@@ -34,7 +46,7 @@ class ContextLogger(object):
         self.logger.debug('[msg] ' + str(msg) + self._context())
 
 
-logger = ContextLogger('root')  # logging.getLogger('test')
+logger = ContextLogger('root', '/')
 
 
 class A(object):
@@ -53,7 +65,11 @@ class A(object):
         try:
             raise Exception('WTF!')
         except Exception as e:
+            logger.critical(e)
             logger.error(e)
+            logger.warning(e)
+            logger.info(e)
+            logger.debug(e)
 
 
 a = A()
